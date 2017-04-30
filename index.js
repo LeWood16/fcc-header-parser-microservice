77153c70f5669be2f95ff140382705380c70e39f
Luke Wood <lewoodwebdev@gmail.com>
2017-04-30 21:14:13 +0000

    switched to a more succinct module for ip

--- index.js
+++ index.js
@@ -2,45 +2,32 @@
 var express = require('express');
 var os = require('os');
 var useragent = require('useragent');
+var publicIp = require('public-ip');
+
 useragent(true);
 
 var app = express();
 var ifaces = os.networkInterfaces();
 var obj = {};
 
-
-// get browser's ip information, then store in in obj
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
-
-  ifaces[ifname].forEach(function (iface) {
-    if ('IPv4' !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
-
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      obj.ifname = alias;
-      obj.ipaddress = iface.address;
-    } else {
-      // this interface has only one ipv4 adress
-      obj.ipaddress = iface.address;
-    }
-    ++alias;
-  });
+// parses public ip, depending in it being either v4 or v6
+publicIp.v4().then(function (ip) {
+    obj.ipaddress = ip;
 });
-    
+
+publicIp.v6().then(function (ip) {
+    obj.ipaddress = ip;
+});
 
 
 app.get('/', function(req, res) {
   
-  // parse browser's selected language
+  // parses browser's selected language
   let lang = req.headers["accept-language"];
   lang = lang.slice(0, lang.indexOf(','));
   obj['language'] = lang;
   
-  // parse OS
+  // parses OS
   var agent = useragent.parse(req.headers['user-agent']);
   obj['software'] = agent.os.toString();
   res.send(obj);
